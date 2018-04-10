c498569e67e7c5cc282a5dfcb7ad7b21d85ea19c
Michael Crowther <you@example.com>
2018-04-10 15:25:14 +0100

    typescript settings for typescript:

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 const Index = () => {
   return <div>Hello React!</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,72 @@
-// webpack.config.js
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
-const path = require('path');
+var path = require('path');
+var webpack = require('webpack');
 
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'my-first-webpack.bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.txt$/, use: 'raw-loader' }
-            , {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' })
-    ]
+module.exports = {
+  entry: [
+    // 'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:3000',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    // 'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './src/index.tsx',
+    // the entry point of our app
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: path.resolve(__dirname, 'dist'),
+
+    publicPath: '/static/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'ts-loader', options: { happyPackMode: true } }
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
+
+
+  ],
+
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+
+    historyApiFallback: true,
+    // respond to 404s with index.html
+
+    hot: true,
+    // enable HMR on the server
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
 };
-
-console.log(`Config outut `, config.output.path);
-
-module.exports = config;
